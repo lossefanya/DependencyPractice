7790a7903ac959a837fe001cb449afdb42df89ee
lossefanya <lossefanya@gmail.com>
2019-01-30 18:44:45 +0100

    init

---
+++ DependencyPractice/DependencyPractice.h
@@ -0,0 +1,19 @@
+//
+//  DependencyPractice.h
+//  DependencyPractice
+//
+//  Created by Yeongweon Park on 30.01.19.
+//  Copyright © 2019 lossefanya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DependencyPractice.
+FOUNDATION_EXPORT double DependencyPracticeVersionNumber;
+
+//! Project version string for DependencyPractice.
+FOUNDATION_EXPORT const unsigned char DependencyPracticeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DependencyPractice/PublicHeader.h>
+
+
---
+++ DependencyPractice/SuperCoolLibrary.swift
@@ -0,0 +1,14 @@
+//
+//  SuperCoolLibrary.swift
+//  DependencyPractice
+//
+//  Created by Yeongweon Park on 30.01.19.
+//  Copyright © 2019 lossefanya. All rights reserved.
+//
+
+import Moya
+import RxSwift
+
+final class SuperCoolLibrary {
+
+}
